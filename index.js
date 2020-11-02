d7949a4eb5874933dca2f3add6a7ede7244eac92
yayatipappala <59293416+yayatipappala@users.noreply.github.com>
2020-11-03 02:02:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,56 @@
+//detecting button press
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    fun(this.innerHTML);
+    pressed(this.innerHTML);
+  })
+}
+//detecting key press
+document.addEventListener("keydown", function() {
+  fun(event.key);
+  pressed(event.key);
+})
+
+function fun(c) {
+  switch (c) {
+    case "w":
+      var crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+    case "a":
+      var kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+    case "s":
+      var snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+    case "d":
+      var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+    case "j":
+      var tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+    case "k":
+      var tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+    case "l":
+      var tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+    default:
+      console.log(c);
+
+  }
+}
+
+function pressed(key) {
+  var activeButton = document.querySelector("." + key);
+  activeButton.classList.add("pressed");
+  setTimeout(function(){
+    activeButton.classList.remove("pressed");
+  },100);
+}
